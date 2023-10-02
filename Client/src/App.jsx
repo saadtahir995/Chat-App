8d616d25b76e000260601a6e5dc6b0685104574d
Saad <saadtahir995@yahoo.com>
2023-10-02 14:45:06 +0500

    minor

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -48,10 +48,7 @@ export default function App() {
               >
                 <span>{msg.socketId}:</span> {msg.message}
               </div>
-            </div>
-            
-            
-}
+            </div>}
               </>
             ))}
           
