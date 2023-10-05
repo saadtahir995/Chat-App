a929f4f14772be30f0b5f5bfa10fc66f6ff1b52f
Saad <saadtahir995@yahoo.com>
2023-10-05 16:29:33 +0500

    added private rooms

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -8,25 +8,36 @@ export default function App() {
   const [msg, setMsg] = useState("");
   const [msgs, setMsgs] = useState([{}]);
   const [name, setName] = useState("");
+  const [room,setRoom]=useState('');
+  const[err,setErr] = useState('');
+  const [isjoined,setIsjoined]=useState(false);
 
   const sendMsg = () => {
-    socket.emit("sending", msg, name);
-    setMsgs((prevmsgs) => [
-      ...prevmsgs,
-      { message: msg, socketId: name ? name : "Me" },
-    ]);
-    setMsg("");
-  };
+    socket.emit("sending", msg, name,room);
+    setMsg("")    
+  }
+  useEffect(()=>{
+    if(name.toLowerCase().includes('alert') || name.toLowerCase().includes('admin') ){setErr(`Word ${name} is forbidden`)}
+    else{setErr('')}
+  },[name])
 
   useEffect(() => {
-    socket.on("response", (msg, id) =>
-      setMsgs((prevmsgs) => [...prevmsgs, { message: msg, socketId: id }])
-    );
-    socket.on("leave", (msg, id) =>
-      setMsgs((prevmsgs) => [...prevmsgs, { message: msg, socketId: id }])
-    );
+    socket.on("response", (msg, id,mainid) =>{
+    id===socket.id&&(id=name?name:'Me')
+      setMsgs((prevmsgs) => [...prevmsgs, { message: msg, socketId: id,main:mainid }])
+  });
   }, []);
-
+ const Handleroom=()=>{
+  setIsjoined(true)
+  setMsgs([{}]);
+  socket.emit("room_join", room,name);
+}
+const HandleLeave=()=>{
+  setIsjoined(false)
+  socket.emit("room_leave",room,name)
+  setMsgs([{}]);
+  setRoom('')
+}
   return (
     <div className="container">
       <h3>Chat App</h3>
@@ -40,7 +51,22 @@ export default function App() {
           placeholder="Enter your name....."
         />
       </div>
+      {err}
+      <div className="name">
+        <input
+          type="text"
+          name="room"
+          id="room"
+          onChange={(e) => setRoom(e.target.value)}
+          value={room}
+          placeholder="Enter room code....."
+        />
+        {room&&(<><button onClick={Handleroom} style={{backgroundColor:'#baffba'}}>Join</button></>)}{isjoined&&(<><button onClick={HandleLeave} style={{backgroundColor:'#ff7e7e'}}>Leave</button></>)}
+      </div>
+      
+
       <div className="chat-box">
+        {isjoined?<><h3>Private Room</h3></>:<><h3>Public Chat</h3></>}
         {msgs.map((msg, index) => (
           <>
             {msg.socketId && (
@@ -49,18 +75,19 @@ export default function App() {
                 className="chat-message"
                 style={{
                   textAlign:
-                    msg.socketId === name || msg.socketId === "Me"
+                  (msg.socketId === name&&msg.main===socket.id) || msg.socketId === "Me"
                       ? "right"
-                      : "left",
+                      :(msg.socketId==='Alert'? "center" : "left") 
                 }}
               >
+
                 <div
                   className="message-content"
                   style={{
                     backgroundColor:
-                      msg.socketId === name || msg.socketId === "Me"
+                      (msg.socketId === name&&msg.main===socket.id) || msg.socketId === "Me"
                         ? "#e9e5e5"
-                        : "#baffba",
+                        :(msg.socketId==='Alert'? "yellow" : "#baffba"),
                     borderRadius: "50px",
                     padding: "3.7px",
                     display: "inline-block",
@@ -81,7 +108,7 @@ export default function App() {
           value={msg}
           onChange={(e) => setMsg(e.target.value)}
         />
-        <button onClick={sendMsg}>Send</button>
+       {!err&&<><button onClick={sendMsg}>Send</button></>}
       </div>
     </div>
   );
--- Server/index.js
+++ Server/index.js
@@ -5,15 +5,31 @@ const io= require('socket.io')(3000,{
 })
 io.on('connection', (socket)=>{
     let id=socket.id;
+    socket.join(0);
 console.log(`client connected with socketid: ${socket.id}`)
-socket.broadcast.emit('response','joined the chat',socket.id);
+io.to(0).emit('response',socket.id+' joined the chat','Alert');
 
-socket.on('sending',(msg,name)=>{
+socket.on('sending',(msg,name,room)=>{
     id=name?name:socket.id
-    socket.broadcast.emit('response',msg,name?name:socket.id);
+    if(!room){io.to(0).emit('response',msg,name?name:socket.id,socket.id)
+}
+    io.to(room).emit('response',msg,name?name:socket.id,socket.id)
+    
+})
+socket.on('room_join',(room,name)=>{
+    socket.leave(0);
+    socket.join(room)
+    io.to(room).emit('response',name?name+' joins room':socket.id +' joins room','Alert')
+    
+})
+socket.on('room_leave',(room,name)=>{
+    io.to(room).emit('response',name?name+' Leaves room':socket.id +' Leaves room','Alert')
+    socket.leave(room)
+    socket.join(0)
+
 })
 socket.on('disconnect',()=>{
-    io.emit('leave','is disconnected',id)
+    io.emit('response',id+' is disconnected','Alert')
 })
 })
 
