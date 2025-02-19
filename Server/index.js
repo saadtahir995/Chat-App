bf9a39e6b24724a121c8c43c64fbc5b850c574dd
Saad <70136884+saadtahir995@users.noreply.github.com>
2025-02-20 02:25:14 +0500

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -84,9 +84,11 @@ io.on('connection', (socket) => {
 })
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+    server.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
 
 module.exports = server;
 
